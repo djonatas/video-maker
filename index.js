05c3decc3a8b519342492988031c25ec1a01b5f2
Djonatas Tenfen <djonatas@live.com>
2019-03-02 09:14:55 -0300

    Input user data

---
+++ index.js
@@ -0,0 +1,23 @@
+const readLine = require('readline-sync');
+
+function start() {
+    const content = {};
+
+    content.searchTerm = askAndReturnSearchTerm();
+    content.prefix = askAndReturnPrefix();
+
+    function askAndReturnSearchTerm(){
+        return readLine.question('Qual termo você busca ? ');
+    }
+
+    function askAndReturnPrefix(){
+        const prefix = ['O que é ?', 'Onde é? ', "Qual a história ?"];
+        const selectedPrefixIndex = readLine.keyInSelect(prefix, "Escolha uma opção para seguir: ");
+
+        return  prefix[selectedPrefixIndex];
+    }
+
+    console.log(content);
+}
+
+start();
